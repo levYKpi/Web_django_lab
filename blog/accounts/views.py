1fa8c306c87bbe3189cd4b0d08e84b29f05c53af
Yasenko_Lev <l.study.kpi@gmail.com>
2021-05-04 18:53:52 +0300

    blog: provided swagger redoc rest_framework

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -2,11 +2,11 @@ from rest_framework import generics
 from . import serializers
 from django.contrib.auth.models import User
 
-class UserList(generics.ListAPIView):
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,9 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+swagger_info = openapi.Info(
+    title="Rest API",
+    default_version='v1',
+    license=openapi.License(name="BSD License"),
+)
+
+
+SchemaView = get_schema_view(
+    info=swagger_info,
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', SchemaView.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
