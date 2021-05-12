b97a27881cdce7f4c37411c2b584892b613f855d
Yasenko_Lev <l.study.kpi@gmail.com>
2021-05-12 17:08:17 +0300

    blog: change APIView to generics

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,18 +1,14 @@
-from django.http.response import HttpResponse
 from rest_framework import generics
 from rest_framework import status
 from . import serializers
 from django.contrib.auth import get_user_model as user, login, logout
-
-from django.urls import reverse_lazy
-from django.views import generic as dvg
 
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class LoginView(APIView):
+class LoginView(generics.GenericAPIView):
     permission_classes = [AllowAny]
     # queryset = user().objects.all()
     serializer_class = serializers.LoginSerializer
@@ -25,7 +21,7 @@ class LoginView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class LogoutView(APIView):
+class LogoutView(generics.GenericAPIView):
     permission_classes = [AllowAny]
     # queryset = user().objects.all()
     serializer_class = serializers.LogoutSerializer
@@ -36,9 +32,10 @@ class LogoutView(APIView):
 
 
 class RegisterView(generics.CreateAPIView):
-    permission_classes = [AllowAny]
+    # permission_classes = [AllowAny]
     queryset = user().objects.all()
     serializer_class = serializers.RegistrationSerializer
+
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
