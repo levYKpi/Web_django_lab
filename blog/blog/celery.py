2ad15812bc1c13bb31b325bad703536648a703f5
Yasenko_Lev <l.study.kpi@gmail.com>
2021-05-27 11:07:00 +0300

    blog: add celery beat

--- blog/blog/celery.py
+++ blog/blog/celery.py
@@ -1,6 +1,9 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+
+from django.contrib.auth import get_user_model
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
@@ -15,3 +18,19 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+@app.task
+def print_count_of_users_registrated():
+    users = get_user_model().objects.all()
+    l = len(users)
+    print("registrated users count is: ", l)
+    return l
+
+
+app.conf.beat_schedule = {
+    'print-count-of-users-registrated': {
+        'task': 'blog.celery.print_count_of_users_registrated',
+        'schedule': crontab(),
+    },
+}
